b86827ddeceaab463b2267168ad41e7da948ef42
mangeshhole1986-glitch <mangeshhole1986@gmail.com>
2026-08-23 17:06:24 +0530

    Fix drug database

--- drug_database.js
+++ drug_database.js
@@ -1,6 +1,1 @@
-const COMMON_DRUGS = [
-  {"no":1,"generic":"Paracetamol","dosage_form":"Tablet","strengths":"500 mg, 650 mg","pack_options":"10, 15, 20, 30 tablets"},
-  {"no":2,"generic":"Ibuprofen","dosage_form":"Tablet","strengths":"200 mg, 400 mg","pack_options":"10, 15, 20 tablets"}
-];
-const CLINICAL_INTERACTIONS = [];
-if (typeof window !== 'undefined') { window.COMMON_DRUGS=COMMON_DRUGS; window.CLINICAL_INTERACTIONS=CLINICAL_INTERACTIONS; }
+const COMMON_DRUGS = "+""+";
