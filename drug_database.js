4d0b43cd54effd4078beade783e934688637db77
mangeshhole1986-glitch <mangeshhole1986@gmail.com>
2026-08-23 17:04:32 +0530

    Fix drug database JavaScript structure

--- drug_database.js
+++ drug_database.js
@@ -1,2 +1,6 @@
-const COMMON_DRUGS="+json.dumps(drugs, ensure_ascii=False, separators=(',',':'))+";
-const CLINICAL_INTERACTIONS="+json.dumps(ints, ensure_ascii=False, separators=(',',':'))+";
+const COMMON_DRUGS = [
+  {"no":1,"generic":"Paracetamol","dosage_form":"Tablet","strengths":"500 mg, 650 mg","pack_options":"10, 15, 20, 30 tablets"},
+  {"no":2,"generic":"Ibuprofen","dosage_form":"Tablet","strengths":"200 mg, 400 mg","pack_options":"10, 15, 20 tablets"}
+];
+const CLINICAL_INTERACTIONS = [];
+if (typeof window !== 'undefined') { window.COMMON_DRUGS=COMMON_DRUGS; window.CLINICAL_INTERACTIONS=CLINICAL_INTERACTIONS; }
